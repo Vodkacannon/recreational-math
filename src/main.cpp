b6509a11adfddfad4661d29243996c00c236f5e7
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2023-01-22 11:33:49 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,21 @@ bool is_palindrome(uint64_t integer) {
   return std::to_string(integer) == reverse_str(std::to_string(integer));  
 }
 
+uint64_t sum_palindromes(uint64_t nth_index) {
+  std::vector<uint64_t> palindromes;
+  uint64_t curr_index = 0;
+  uint64_t number_to_check = 0;
+  
+  while (curr_index < nth_index) {
+    if (is_palindrome(number_to_check)) {
+      palindromes.push_back();
+      curr_index++;
+    }
+  }
+  
+  return sum(palindromes);
+}
+
 int main() {
   return 0;
 }
